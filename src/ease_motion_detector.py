e501ab279fdfcede90daa19c67b74d4dfe8418a5
Kevin <kevinleeex@foxmail.com>
2018-08-15 20:07:57 +0800

    add ease motion detector & 3f based motion detector

---
+++ src/ease_motion_detector.py
@@ -0,0 +1,143 @@
+#!/usr/bin/env python
+# encoding: utf-8
+import cv2
+import numpy as np
+import time
+
+"""avg_motion_detector.
+This is average motion detection algorithm.
+
+Example:
+        $ python avg_motion_detector.py
+
+Todo:
+    * For module TODOs
+
+"""
+__author__ = "Kevin Lee"
+__affiliate__ = "lidengju.com"
+__copyright__ = "Copyright 2018, Kevin"
+__license__ = "MIT"
+__version__ = "0.1"
+__maintainer__ = "Kevin Lee"
+__email__ = "kevinleeex@foxmail.com"
+__status__ = "Development"
+
+
+class AvgMotionDetector():
+    def onChanged(self, val):
+        self.threshold = val
+
+    def __init__(self, threshold=25, is_record=True, show_window=True):
+        self.writer = None
+        self.font = None
+        self.threshold = threshold
+        self.is_record = is_record
+        self.init_flag = True
+        self.show_window = show_window
+        self.frame = None
+        self.capture = cv2.VideoCapture(0)
+        self.frame = self.capture.read()[1]
+        self.frame_w = self.frame.shape[1]  # frame width
+        self.frame_h = self.frame.shape[0]  # frame height
+        self.gray_frame = cv2.cvtColor(self.frame, cv2.COLOR_BGR2GRAY)
+        self.average_frame = np.float32(self.frame)
+        self.absdiff_frame = None
+        self.previous_frame = None
+        self.frame_area = self.frame_w * self.frame_h  # frame area
+
+        self.cur_contour = None
+        self.cur_area = 0
+        self.is_recording = False
+        self.trigger_time = 0  # timestamp of last detection
+
+        # show window with track bar.
+        if show_window:
+            self.window_name = "Motion Detection Result"
+            cv2.namedWindow(self.window_name)
+            cv2.createTrackbar("Threshold", self.window_name, self.threshold, 100, self.onChanged)
+
+    def initRecorder(self):
+        """
+        Init the video recorder
+        """
+        fourcc = cv2.VideoWriter_fourcc(*"MPEG")
+        self.writer = cv2.VideoWriter('output2.avi', fourcc, 20, (640, 480))
+
+    def run(self):
+        started_time = time.time()
+        while True:
+            cur_frame = self.capture.read()[1]
+            ts_frame = time.time()  # time of current frame.
+
+            t_img = self.processFrame(cur_frame)
+            # cv2.imshow("Test", t_img)
+            if self.hasMoved():
+                self.trigger_time = ts_frame
+                print("Object moved")
+            cv2.drawContours(cur_frame, self.cur_contour, -1, (0, 0, 255), 2)
+
+            if self.show_window:
+                cv2.imshow(self.window_name, cur_frame)
+            key = cv2.waitKey(100) % 0x100
+            if key == 27:
+                break
+
+    def processFrame(self, cur_frame):
+        """
+        Image processing, blur--> compute diff--> gray_frame--> dilation--> eroding
+        :param cur_frame: current frame
+        :return: result
+        """
+        cv2.blur(cur_frame, (3, 3), cur_frame)
+        # first time to compute
+        if self.init_flag:
+            self.init_flag = False
+            self.absdiff_frame = cur_frame.copy()
+            self.previous_frame = cur_frame.copy()
+            # self.average_frame = cv2.convertScaleAbs(cur_frame)
+        else:
+            cv2.accumulateWeighted(cur_frame, self.average_frame, 0.05)  # compute average
+        self.previous_frame = cv2.convertScaleAbs(self.average_frame)
+        self.absdiff_frame = cv2.absdiff(cur_frame, self.previous_frame)  # abs(average - cur_frame)
+
+        self.gray_frame = cv2.cvtColor(self.absdiff_frame, cv2.COLOR_BGR2GRAY)
+        cv2.threshold(self.gray_frame, 50, 255, cv2.THRESH_BINARY, self.gray_frame)
+
+        # define kernels
+        dilate_kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (15, 15))
+        erode_kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (10, 10))
+        cv2.dilate(self.gray_frame, dilate_kernel, self.gray_frame)  # dilation
+        result = cv2.erode(self.gray_frame, erode_kernel, self.gray_frame)  # eroding
+        return result
+
+    def hasMoved(self):
+        """
+        Judge the object is moving or not.
+        :return: is_moved(boolean)
+        """
+        out, contours, hierarchy = cv2.findContours(self.gray_frame, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+        self.cur_contour = contours
+        for i in range(len(contours)):
+            c = cv2.contourArea(contours[i])
+            self.cur_area += c
+
+        val_area = (self.cur_area * 100) / self.frame_area  # calculate the ratio of cur_area/frame_area
+        self.cur_area = 0
+
+        if val_area > self.threshold:
+            return True
+        else:
+            return False
+
+    def cca(self, src_img):
+        """
+        connected component analysis
+        :param src_img: source image to analysis
+        :return:
+        """
+
+
+if __name__ == '__main__':
+    detect = AvgMotionDetector()
+    detect.run()
---
+++ src/motion_detector_3f.py
@@ -0,0 +1,74 @@
+#!/usr/bin/env python
+# encoding: utf-8
+import cv2
+import numpy as np
+"""
+This is the 2 frame based motion detection algorithm.
+
+Todo:
+    * Finish code.
+
+"""
+__author__ = "Kevin Lee"
+__affiliate__ = "lidengju.com"
+__copyright__ = "Copyright 2018, Kevin"
+__license__ = "MIT"
+__version__ = "0.1"
+__maintainer__ = "Kevin Lee"
+__email__ = "kevinleeex@foxmail.com"
+__status__ = "Development"
+
+
+def diffImg(t0, t1):
+    d2 = cv2.absdiff(t1, t0)
+    return d2
+
+
+cam = cv2.VideoCapture(0)
+
+winName = "Motion Detector"
+cv2.namedWindow(winName, cv2.WINDOW_AUTOSIZE)
+
+# Read two images first:
+t_minus = cv2.cvtColor(cam.read()[1], cv2.COLOR_RGB2GRAY)
+t_img = cam.read()[1]
+t_source = np.copy(t_img)
+t = cv2.cvtColor(t_img, cv2.COLOR_RGB2GRAY)
+
+while True:
+    diff_img = diffImg(t_minus, t)
+    ret, thresh_img = cv2.threshold(diff_img, 50, 255, cv2.THRESH_BINARY)
+    kernel_erode = cv2.getStructuringElement(cv2.MORPH_RECT, (3, 3))
+    kernel_dilate = cv2.getStructuringElement(cv2.MORPH_RECT, (20, 20))
+    erode_img = cv2.erode(thresh_img, kernel_erode)
+    dilate_img = cv2.dilate(erode_img, kernel_dilate)
+    out_binary, contours, hierarchy = cv2.findContours(dilate_img, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    c_max = []
+    for i in range(len(contours)):
+        cnt = contours[i]
+        area = cv2.contourArea(cnt)
+
+        # reduce the small region.
+        if (area < 100):
+            c_min = []
+            c_min.append(cnt)
+            #  fill the small blob in black.
+            cv2.drawContours(dilate_img, c_min, -1, (0, 0, 0), thickness=-1)
+            continue
+        c_max.append(cnt)
+
+    cv2.drawContours(t_source, c_max, -1, (0, 0, 255), thickness=2)
+    cv2.imshow(winName, t_source)
+
+    # Read next image
+    t_minus = t
+    cur_img = cam.read()[1]
+    t_source = cur_img
+    t = cv2.cvtColor(cur_img, cv2.COLOR_BGR2GRAY)
+
+    key = cv2.waitKey(25)
+    if key == 27:
+        cv2.destroyWindow(winName)
+        break
+
+print("Goodbye")
