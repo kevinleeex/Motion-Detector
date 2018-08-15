03e89a38bf0eee694f6b9f23bdc4971d519e5975
Kevin <kevinleeex@foxmail.com>
2018-08-15 11:45:08 +0800

    add 2 frame based motion detector

---
+++ src/__init__.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+
+# encoding: utf-8
+
+"""Example Google style docstrings.
+
+This module demonstrates documentation as specified by the `Google Python
+Style Guide`_. Docstrings may extend over multiple lines. Sections are created
+with a section header and a colon followed by a block of indented text.
+
+Example:
+    Examples can be given using either the ``Example`` or ``Examples``
+    sections. Sections support any reStructuredText formatting, including
+    literal blocks::
+
+        $ python example_google.py
+
+Section breaks are created by resuming unindented text. Section breaks
+are also implicitly created anytime a new section starts.
+
+Attributes:
+    module_level_variable1 (int): Module level variables may be documented in
+        either the ``Attributes`` section of the module docstring, or in an
+        inline docstring immediately following the variable.
+
+        Either form is acceptable, but the two should not be mixed. Choose
+        one convention to document module level variables and be consistent
+        with it.
+
+Todo:
+    * For module TODOs
+
+"""
+__author__ = "Kevin Lee"
+__affiliate__ = "Neuromorphic Computing Research Center of Sichuan Univ."
+__copyright__ = "Copyright 2018, Kevin"
+__license__ = "MIT"
+__version__ = "0.1"
+__maintainer__ = "Kevin Lee"
+__email__ = "kevinleeex@foxmail.com"
+__status__ = "Development"
