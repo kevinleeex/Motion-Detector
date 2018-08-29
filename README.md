476ae1a558eafc31c09847ec9ba025fd67460bd5
Kevin <kevinleeex@foxmail.com>
2018-08-29 11:23:15 +0800

    add motion attention detector

--- README.md
+++ README.md
@@ -4,10 +4,16 @@
 Three kinds of motion detection methods are provided.
 - motion_detector_2f
   > Motion detector based on 2 frame, showed more diff regions, but it's not suitable for fast moving.
+
 - motion_detector_3f
   > Motion detector based on 3 frame, showed less diff regions, but it's more suitable for fast moving. 
+
 - ease_motion_detector
   > Motion detector with ease affects, that means the detected motion contour would not disappear at once.
+
+- motion_attention_point
+
+  > This is based on ease motion detector and could detect the attention point according to the motion.
 
 ## Requirements 
 -  opencv-python 3.x
--- src/motion_attention_point.py
+++ src/motion_attention_point.py
@@ -3,11 +3,11 @@
 import cv2
 import numpy as np
 
-"""ease_motion_detector.
-This is ease motion detection algorithm.
+"""motion_attention_point_detector.
+This is ease motion attention point calculation algorithm.
 
 Example:
-        $ python ease_motion_detector.py
+        $ python motion_attention_point.py
 
 Todo:
     * cca function
@@ -23,11 +23,12 @@ __email__ = "kevinleeex@foxmail.com"
 __status__ = "Development"
 
 
-class EaseMotionDetector():
+class MotionAttention:
     def onChanged(self, val):
         self.diff_thresh = val
 
     def __init__(self, diff_thresh=25, move_sense=0.05, show_window=True):
+        self.contour_area = []
         self.writer = None
         self.font = None
         self.diff_thresh = diff_thresh
@@ -35,16 +36,14 @@ class EaseMotionDetector():
         self.init_flag = True
         self.show_window = show_window
         self.frame = None
-        self.capture = cv2.VideoCapture(0)
-        self.frame = self.capture.read()[1]
-        self.frame_w = self.frame.shape[1]  # frame width
-        self.frame_h = self.frame.shape[0]  # frame height
-        self.frame_area = self.frame_w * self.frame_h  # frame area
-        self.gray_frame = cv2.cvtColor(self.frame, cv2.COLOR_BGR2GRAY)
-        self.fusion_frame = np.float32(self.frame)
+        self.frame_w = None  # frame width
+        self.frame_h = None  # frame height
+        self.frame_area = None  # frame area
+        self.gray_frame = None
+        self.fusion_frame = None
         self.absdiff_frame = None
         self.previous_frame = None
-
+        self.attention_point = None
         self.cur_contour = None
         self.cur_area = 0
 
@@ -54,21 +53,44 @@ class EaseMotionDetector():
             cv2.namedWindow(self.window_name)
             cv2.createTrackbar("Difference Threshold", self.window_name, self.diff_thresh, 100, self.onChanged)
 
-    def run(self):
-        while True:
-            cur_frame = self.capture.read()[1]
-            self.processFrame(cur_frame)
+    def setFrame(self):
+        """
+        Init the frame setting.
+        :return:
+        """
+        self.frame_w = self.frame.shape[1]  # frame width
+        self.frame_h = self.frame.shape[0]  # frame height
+        self.frame_area = self.frame_w * self.frame_h  # frame area
+        self.gray_frame = cv2.cvtColor(self.frame, cv2.COLOR_BGR2GRAY)
+        self.fusion_frame = np.float32(self.frame)
 
-            if self.hasMoved():
-                print("Object moved")
-            cv2.imshow("Diff", self.gray_frame)
+    def run(self, frame):
+        """
+        one run
+        :param frame: current frame
+        :return: current frame with contours, gray frame with motion variation, attention point
+        """
+        if self.init_flag:
+            self.frame = frame
+            self.setFrame()
+        cur_frame = frame
+        diff_frame = cv2.cvtColor(self.gray_frame, cv2.COLOR_GRAY2BGR)
+        self.processFrame(cur_frame)
+
+        if self.hasMoved():
+            print("Object moved")
+            self.attention_point = self.calAttentionPoint()
+            print(self.attention_point)
+            # draw attention point
+            cv2.circle(cur_frame, (self.attention_point[0], self.attention_point[1]), 5, (0, 255, 0), -1)
+            cv2.circle(diff_frame, (self.attention_point[0], self.attention_point[1]), 5, (0, 255, 0), -1)
+
+        if self.show_window:
             cv2.drawContours(cur_frame, self.cur_contour, -1, (0, 0, 255), 2)
+            cv2.imshow(self.window_name, cur_frame)
+            cv2.imshow("Diff", diff_frame)
 
-            if self.show_window:
-                cv2.imshow(self.window_name, cur_frame)
-            key = cv2.waitKey(50) % 0x100
-            if key == 27:
-                break
+        return cur_frame, self.gray_frame, self.attention_point
 
     def processFrame(self, cur_frame):
         """
@@ -82,7 +104,6 @@ class EaseMotionDetector():
             self.init_flag = False
             self.absdiff_frame = cur_frame.copy()
             self.previous_frame = cur_frame.copy()
-            # self.average_frame = cv2.convertScaleAbs(cur_frame)
         else:
             cv2.accumulateWeighted(cur_frame, self.fusion_frame, 0.05)  # compute average
         self.previous_frame = cv2.convertScaleAbs(self.fusion_frame)
@@ -106,22 +127,48 @@ class EaseMotionDetector():
         out, contours, hierarchy = cv2.findContours(self.gray_frame, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
         self.cur_contour = contours
         max_contour = []
+        contour_areas = []  # area of contours
+        self.cur_area = 0
         for i in range(len(contours)):
             c = cv2.contourArea(contours[i])
             if c < 0.005 * self.frame_area:
                 # fill small white blob in black
                 cv2.drawContours(self.gray_frame, contours, i, (0, 0, 0), -1)
             else:
+                contour_areas.append(c)
                 max_contour.append(contours[i])
-            self.cur_area += c
+                self.cur_area += c
         self.cur_contour = max_contour
+        self.contour_area = contour_areas
         val_area = self.cur_area / self.frame_area  # calculate the ratio of cur_area/frame_area
-        self.cur_area = 0
 
         if val_area > self.move_sense:
             return True
         else:
             return False
+
+    def calAttentionPoint(self):
+        """
+        Calculate the weighted mean attention point.
+        :return: attention point
+        """
+        # get center coord of contours
+        centroids = []
+        area_ratios = []
+        for ix, contour in enumerate(self.cur_contour):
+            mm = cv2.moments(contour)
+            cx = int(mm['m10'] / mm['m00'])
+            cy = int(mm['m01'] / mm['m00'])
+            center = [cx, cy]
+            a_ratio = 0
+            if self.cur_area != 0:
+                # print(self.cur_area)
+                a_ratio = self.contour_area[ix] / self.cur_area
+            centroids.append(center)
+            area_ratios.append(a_ratio)
+
+        attention_point = np.dot(np.array(area_ratios).transpose(), np.array(centroids)).squeeze()
+        return np.int32(attention_point)
 
     def cca(self, src_img):
         """
@@ -135,5 +182,11 @@ class EaseMotionDetector():
 
 
 if __name__ == '__main__':
-    detect = EaseMotionDetector(diff_thresh=40)
-    detect.run()
+    detect = MotionAttention(diff_thresh=40, move_sense=0.01)
+    capture = cv2.VideoCapture(0)
+    while True:
+        frame = capture.read()[1]
+        detect.run(frame)
+        c = cv2.waitKey(1) & 0x100
+        if c == 27:
+            break
